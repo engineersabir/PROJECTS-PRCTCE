5f0289d7187a3d31eeea5d388deeaf736633f14c
sabir-pseudo <sabirfareedmalik@gmail.com>
2025-09-13 10:36:48 +0500

    Update index.html and style.css

--- form validation/script.js
+++ form validation/script.js
@@ -3,3 +3,10 @@ var phoneError=document.getElementById('phone-error');
 var messageError=document.getElementById('message-error');
 var emailError=document.getElementById('email-error');
 var submitError=document.getElementById('submit-error');
+
+function validateName(){
+    var name =document.getElementById('contact-name').value;
+if(name.length==0){
+    nameError.innerHTML='Name is required';
+}
+} 
