5de4947e997749d7986bd3f731a576b1081f4715
sabir-pseudo <sabirfareedmalik@gmail.com>
2025-09-14 20:02:20 +0500

    Update index.html and style.css

--- form validation/script.js
+++ form validation/script.js
@@ -34,3 +34,16 @@ function validatePhone(){
    phoneError.innerHTML=' no. entered successfully';
    return true; 
 }
+function validateEmail(){
+    var email=document.getElementById('contact-email').value;
+    if(email.length==0){
+        emailError.innerHTML='Email is required'
+        return false;
+    }
+    if(!email.match(/^[A-Za-z]\._\-[0-9]*[@][A-Za-z]*[\.][a-z]{2,4}$/)){
+        emailError.innerHTML='email is invalid'
+        return false;
+    }
+    emailError.innerHTML=' how are you ';
+    return true;
+}
