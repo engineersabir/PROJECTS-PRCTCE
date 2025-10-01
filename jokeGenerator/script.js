2dcf00c182f64f4c7818fc079c70edbefb526514
sabir-pseudo <sabirfareedmalik@gmail.com>
2025-10-01 18:44:09 +0500

    Update index.html and style.css

---
+++ jokeGenerator/script.js
@@ -0,0 +1,14 @@
+const jokeContainer=document.getElementById('joke');
+const btn=document.getElementById('btn');
+const url="https://v2.jokeapi.dev/joke/Programming,Miscellaneous,Dark?blacklistFlags=nsfw,political,racist,explicit&type=single";
+let getJoke=()=>{
+    jokeContainer.classList.remove("fade");
+    fetch(url)
+    .then(data=>data.json())
+    .then(item=>{
+        jokeContainer.textContent=`${item.joke}`;
+        jokeContainer.classList.add("fade");
+    });
+}
+btn.addEventListener('click',getJoke);
+getJoke();
