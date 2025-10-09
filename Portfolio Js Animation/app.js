82e6fd44e6b3e9f3ad77f84f7ced0dae9fdfc297
sabir-pseudo <sabirfareedmalik@gmail.com>
2025-10-09 21:26:31 +0500

    Update index.html and style.css

--- Portfolio Js Animation/app.js
+++ Portfolio Js Animation/app.js
@@ -1,3 +1,4 @@
 const video1=document.getElementById("project-video1");
 const video2=document.getElementById("project-video2");
 const video3=document.getElementById("project-video3");
+const vProject=document.getElementById("abc");
