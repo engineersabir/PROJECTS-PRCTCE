594a8bda9d619a95c0086db147e6599e4d36f359
sabir-pseudo <sabirfareedmalik@gmail.com>
2025-10-11 16:47:38 +0500

    Update index.html and style.css

--- Portfolio Js Animation/app.js
+++ Portfolio Js Animation/app.js
@@ -3,6 +3,12 @@ const video2=document.getElementById("project-video2");
 const video3=document.getElementById("project-video3");
 const hoverSign=document.querySelector(".hover-sign");
 const videoList=[video1,video2,video3];
+
+// Side Bar elements
+const sideBar=document.querySelector('.side-bar');
+const menu=document.querySelector('.menu-icons');
+const close=document.querySelector('.close-icon');
+
 videoList.forEach(function(video){
     video.addEventListener('mouseover',function(){
         video.onplay();
@@ -14,6 +20,14 @@ videoList.forEach(function(video){
     })
 })
 
+sideBar.addEventListener('click', function(){
+    sideBar.classList.remove('close-sidebar');
+    sideBar.classList.add("open-side-bar");
+})
 
+close.addEventListener('click',function(){
+    sideBar.classList.remove('open-side-bar');
+    sideBar.classList.add("close-sidebar");
+})
 
 
