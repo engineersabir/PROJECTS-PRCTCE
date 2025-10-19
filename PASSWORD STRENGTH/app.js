a166fa3af21d241c2cfa61acaa724f9fd4a0796f
sabir-pseudo <sabirfareedmalik@gmail.com>
2025-10-19 11:14:07 +0500

    Update index.html and style.css

--- PASSWORD STRENGTH/app.js
+++ PASSWORD STRENGTH/app.js
@@ -4,5 +4,18 @@ var str=document.getElementById("strength");
 
 
 pass.addEventListener('input',()=>{
-    
+    if(pass.ariaValueMax.length>0){
+        msg.style.display="block";
+    }else{
+        msg.style.display="none";
+    }
+    if(pass.value.length<4){
+        str.innerHTML="weak";
+    }
+    else if(pass.value.length>=4 && pass.value.length<8){
+        str.innerHTML="medium";
+    }
+    else if(pass.value.length>=8){
+        str.innerHTML="strong";
+    }
 })
