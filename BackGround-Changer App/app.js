cbfe857582dce42f7c2c5d49dda4d03c144bf1d0
sabir-pseudo <sabirfareedmalik@gmail.com>
2025-10-22 21:32:27 +0500

    Update index.html and style.css

---
+++ BackGround-Changer App/app.js
@@ -0,0 +1,8 @@
+var imgBox = document.querySelector(".img-box");
+var imgWrap = document.querySelector(".img-wrap");
+var leftSpace = imgBox.offsetLeft;
+
+imgBox.onmousemove=function(e){
+    var boxWidth=(e.pageX-leftSpace)+"px";
+    imgWrap.computedStyleMap.width
+}
