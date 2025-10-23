d38a9c2c91cc0a46b988a59f3f6c8b5e2918ca33
sabir-pseudo <sabirfareedmalik@gmail.com>
2025-10-23 15:04:36 +0500

    Update index.html and style.css

--- BackGround-Changer App/app.js
+++ BackGround-Changer App/app.js
@@ -1,11 +1,16 @@
 var imgBox = document.querySelector(".img-box");
 var imgWrap = document.querySelector(".img-wrap");
-var leftSpace = imgBox.offsetLeft;
-var originalImg = document.getElementById("originalImg");
 var line = document.getElementById("line");
 
-imgBox.onmousemove = function (e) {
-  var boxWidth = e.pageX - leftSpace + "px";
-  imgWrap.style.width=boxWidth;
-  line.style.left=boxWidth;
-};
+imgBox.addEventListener("mousemove", function(e) {
+  var boxLeft = imgBox.getBoundingClientRect().left;
+  var boxWidth = imgBox.offsetWidth;
+  var position = e.pageX - boxLeft;
+
+  // Clamp to prevent overflow
+  if (position < 0) position = 0;
+  if (position > boxWidth) position = boxWidth;
+
+  imgWrap.style.width = position + "px";
+  line.style.left = position + "px";
+});
