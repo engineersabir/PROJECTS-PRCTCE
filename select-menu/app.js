eaa899e4eb48dd4ffeb3f1b3da34322f4119e5b6
sabir-pseudo <sabirfareedmalik@gmail.com>
2025-10-27 22:35:48 +0500

    Update index.html and style.css

--- select-menu/app.js
+++ select-menu/app.js
@@ -1,3 +1,19 @@
 var selectfield=document.getElementById("selectfield");
 var selecttext=document.getElementById("selecttext");
 var options=document.getElementsByClassName("options");
+var list=document.getElementById("list");
+var arrowIcon=document.getElementById("arrowIcon");
+
+selectfield.onclick=function(){
+    list.classList.toggle("hide");
+    arrowIcon.classList.toggle("rotate");
+    
+}
+
+for(Option of options){
+    Option.onclick=function(){
+        selecttext.innerHTML=this.textContent;
+        list.classList.toggle("hide");
+        arrowIcon.classList.toggle("rotate");
+    }
+}
