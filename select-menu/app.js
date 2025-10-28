948035bc66c1de40bb5186db2ee6a8ef0667d6fd
sabir-pseudo <sabirfareedmalik@gmail.com>
2025-10-28 14:11:37 +0500

    Update index.html and style.css

--- select-menu/app.js
+++ select-menu/app.js
@@ -1,19 +1,18 @@
-var selectfield=document.getElementById("selectfield");
-var selecttext=document.getElementById("selecttext");
-var options=document.getElementsByClassName("options");
-var list=document.getElementById("list");
-var arrowIcon=document.getElementById("arrowIcon");
+var selectfield = document.getElementById("selectfield");
+var selecttext = document.getElementById("selecttext");
+var options = document.getElementsByClassName("options");
+var list = document.getElementById("list");
+var arrowIcon = document.getElementById("arrowIcon");
 
-selectfield.onclick=function(){
-    list.classList.toggle("hide");
-    arrowIcon.classList.toggle("rotate");
-    
-}
+selectfield.onclick = function () {
+  list.classList.toggle("hide");
+  arrowIcon.classList.toggle("rotate");
+};
 
-for(Option of options){
-    Option.onclick=function(){
-        selecttext.innerHTML=this.textContent;
-        list.classList.toggle("hide");
-        arrowIcon.classList.toggle("rotate");
-    }
+for (let option of options) {
+  option.onclick = function () {
+    selecttext.innerHTML = this.textContent;
+    list.classList.add("hide");
+    arrowIcon.classList.remove("rotate");
+  };
 }
