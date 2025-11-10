bb20876c78f0a01e85f474ea5f2c2eb95bbcb604
sabir-pseudo <sabirfareedmalik@gmail.com>
2025-11-10 13:21:20 +0500

    Update index.html and style.css

---
+++ Image_Search_engine/file.js
@@ -0,0 +1,13 @@
+const searchForm=document.getElementById("search-form");
+const searchBox=document.getElementById("search-box");
+const searchResult=document.getElementById("search-result");
+const showMoreBtn=document.getElementById("show-more-btn");
+
+let keyword="";
+let page=1;
+
+async function searchImages() {
+    keyword=searchBox.value;
+    const url=''
+    
+}
