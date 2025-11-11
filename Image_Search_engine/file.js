f6fb4b72c6d5b740571c1a0c5fe83ebfa42c6346
sabir-pseudo <sabirfareedmalik@gmail.com>
2025-11-11 12:04:22 +0500

    Update index.html and style.css

--- Image_Search_engine/file.js
+++ Image_Search_engine/file.js
@@ -1,3 +1,5 @@
+const accessKey="r4wOhD190JOWFQqsf33Qj68OWIKE8RemyrayojLaS9s";
+
 const searchForm=document.getElementById("search-form");
 const searchBox=document.getElementById("search-box");
 const searchResult=document.getElementById("search-result");
@@ -8,6 +10,15 @@ let page=1;
 
 async function searchImages() {
     keyword=searchBox.value;
-    const url=''
+    const url=`https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=${accessKey} `;
     
+    const repsonse=await fetch(url);
+    const data = await repsonse.json();
+    console.log(data);
 }
+searchForm.addEventListener("submit",(e)=>{
+
+    e.preventDefault();
+    page=1;
+    searchImages();
+})
