8e23a575c23542d3724bae56d6548f1bbb357e0e
sabir-pseudo <sabirfareedmalik@gmail.com>
2025-11-12 10:52:04 +0500

    Update index.html and style.css

--- Image_Search_engine/file.js
+++ Image_Search_engine/file.js
@@ -12,12 +12,19 @@ async function searchImages() {
     keyword=searchBox.value;
     const url=`https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=${accessKey} `;
     
-    const repsonse=await fetch(url);
-    const data = await repsonse.json();
+    const response=await fetch(url);
+    const data = await response.json();
 
     const results=data.results;
     results.map((result)=>{
-        image.src=results
+        const image=document.createElement("img");
+        image.src=result.urls.small;
+        const imageLink=document.createElement("a");
+        imageLink.href=result.links.html;
+        imageLink.target="_blank";
+
+        imageLink.appendChild(image);
+        searchResult.appendChild(imageLink); 
     })
 }
 searchForm.addEventListener("submit",(e)=>{
