85d66ca1cd67e1b9869a9627928d407b80a34622
sabir-pseudo <sabirfareedmalik@gmail.com>
2025-11-20 11:04:43 +0500

    Update index.html and style.css

--- Multiplication_App/app.js
+++ Multiplication_App/app.js
@@ -1,1 +1,39 @@
-const num1=Math.random();
+const num1 = Math.ceil(Math.random() * 10);
+const num2 = Math.ceil(Math.random() * 10);
+
+const questionEl = document.getElementById("question");
+const scoreEl = document.getElementById("score");
+
+let score = JSON.parse(localStorage.getItem("score"));
+
+if (!score) {
+    score = 0;
+}
+
+scoreEl.innerText = `score: ${score}`;
+questionEl.innerText = `What is ${num1} multiply by ${num2}?`;
+
+const formEl = document.getElementById("form");
+const inputEl = document.getElementById("input");
+
+const correctAns = num1 * num2;
+
+formEl.addEventListener("submit", (event) => {
+    event.preventDefault(); // stop form from refreshing page
+
+    const userAns = Number(inputEl.value); // get actual input value
+
+    if (userAns === correctAns) {
+        score++;
+    } else {
+        score--;
+    }
+
+    updateLocalStorage();
+    scoreEl.innerText = `score: ${score}`;
+    inputEl.value = ""; // clear the input
+});
+
+function updateLocalStorage() {
+    localStorage.setItem("score", JSON.stringify(score));
+}
