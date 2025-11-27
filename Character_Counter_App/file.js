c882610d67438f2a9215fb13cec02683ffa37d0f
sabir-pseudo <sabirfareedmalik@gmail.com>
2025-11-27 14:38:33 +0500

    Update index.html and style.css

--- Character_Counter_App/file.js
+++ Character_Counter_App/file.js
@@ -1,9 +1,10 @@
 const texteareaEl=document.getElementById("textarea");
 const totalCounterEl=document.getElementById("total-counter");
-
+const remainingCounterEl=document.getElementById("remaining-counter");
 texteareaEl.addEventListener("keyup", ()=>{
     updateCounter()
 })
 function updateCounter(){
     texteareaEl.innerText=texteareaEl.ariaValueMax.length
+
 }
