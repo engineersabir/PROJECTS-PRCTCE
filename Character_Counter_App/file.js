04242ad1288854a6d3e0dbcf96b8c693e15712fa
sabir-pseudo <sabirfareedmalik@gmail.com>
2025-11-27 14:43:22 +0500

    Update index.html and style.css

--- Character_Counter_App/file.js
+++ Character_Counter_App/file.js
@@ -1,10 +1,14 @@
-const texteareaEl=document.getElementById("textarea");
-const totalCounterEl=document.getElementById("total-counter");
-const remainingCounterEl=document.getElementById("remaining-counter");
-texteareaEl.addEventListener("keyup", ()=>{
-    updateCounter()
-})
-function updateCounter(){
-    texteareaEl.innerText=texteareaEl.ariaValueMax.length
+const textareaEl = document.getElementById("textarea");
+const totalCounterEl = document.getElementById("total-counter");
+const remainingCounterEl = document.getElementById("remaining-counter");
 
+textareaEl.addEventListener("keyup", () => {
+    updateCounter();
+});
+
+updateCounter();
+
+function updateCounter() {
+    totalCounterEl.innerText = textareaEl.value.length; 
+    remainingCounterEl.innerText = textareaEl.getAttribute("maxlength") - textareaEl.value.length;
 }
