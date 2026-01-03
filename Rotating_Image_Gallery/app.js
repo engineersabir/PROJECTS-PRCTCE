26da04c0a829257ebe4fa7491fe0869bb749067a
sabir-pseudo <sabirfareedmalik@gmail.com>
2026-01-03 17:03:06 +0500

    Update index.html and style.css

--- Rotating_Image_Gallery/app.js
+++ Rotating_Image_Gallery/app.js
@@ -1,3 +1,29 @@
-const imageContainer=document.querySelector(".imagecontainer");
-const prevEl=document.getElementById("pre");
-const nextEl=document.getElementById("next");
+const imageContainer = document.querySelector(".imagecontainer");
+const prevEl = document.getElementById("pre");
+const nextEl = document.getElementById("next");
+
+let x = 0;
+let timer;
+
+prevEl.addEventListener("click", () => {
+    x += 45;
+    clearTimeout(timer);
+    updateGallery();
+});
+
+nextEl.addEventListener("click", () => {
+    x -= 45;
+    clearTimeout(timer);
+    updateGallery();
+});
+
+function updateGallery() {
+    imageContainer.style.transform = `perspective(1000px) rotateY(${x}deg)`;
+
+    timer = setTimeout(() => {
+        x -= 45;
+        updateGallery();
+    }, 3000);
+}
+
+updateGallery();
